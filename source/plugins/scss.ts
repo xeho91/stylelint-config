9448019303e60c650d5d49623a5bf10b12043bb0
xeho91 <xeho91@pm.me>
2021-08-09 12:56:03 +0800

    fix(Scss): 🐛  tweak rules

--- source/plugins/scss.ts
+++ source/plugins/scss.ts
@@ -3,8 +3,14 @@ import type { Configuration } from "stylelint";
 // https://github.com/kristerkari/stylelint-scss
 const config: Partial<Configuration> = {
 	plugins: [
-		"scss",
+		"stylelint-scss",
 	],
+
+	rules: {
+		"at-rule-no-unknown": null,
+		"no-invalid-position-at-import-rule": null,
+		"scss/at-rule-no-unknown": true,
+	},
 };
 
 export default config;
