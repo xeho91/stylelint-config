cb2004d99027a0a3b3567d86808c48cbe90aebfa
xeho91 <xeho91@pm.me>
2021-08-09 12:30:40 +0800

    fix: 🐛  wrong requires paths

--- source/index.ts
+++ source/index.ts
@@ -4,17 +4,17 @@ import merge from "deepmerge";
 import hasModule from "./helpers/has-module";
 
 const configurations = [
-	require("stylelint").default,
-	require("plugins/8pointGrid").default,
-	require("plugins/A11y").default,
-	require("plugins/ColorFormat").default,
-	require("plugins/CSStreeValidator").default,
-	require("plugins/DeclarationStrictValue").default,
-	require("plugins/HighPerformanceAnimation").default,
-	require("plugins/NoUnsupportedBrowserFeatures").default,
-	require("plugins/Order").default,
-	hasModule("prettier") && require("plugins/Prettier").default,
-	hasModule("sass") && require("plugins/Scss").default,
+	require("./stylelint").default,
+	require("./plugins/8-point-grid").default,
+	require("./plugins/a11y").default,
+	require("./plugins/color-format").default,
+	require("./plugins/csstree-validator").default,
+	require("./plugins/declaration-strict-value").default,
+	require("./plugins/high-performance-animation").default,
+	require("./plugins/no-unsupported-browser-features").default,
+	require("./plugins/order").default,
+	hasModule("prettier") && require("./plugins/prettier").default,
+	hasModule("sass") && require("./plugins/scss").default,
 ];
 
 const config = merge.all(configurations);
